c035a631c8032ed4ce7c71375bc2a827d0e51e15
Giuseppe Barbieri <elect86@gmail.com>
2020-09-03 16:20:01 +0200

    - renamed appending -jdk8 - removed netlify - optimized settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,6 @@
 rootProject.name = "kool-jdk8"
 
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
-    }
+pluginManagement.repositories {
+    gradlePluginPortal()
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
 }
