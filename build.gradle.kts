0beeb3521511b83c8cb78ffdfdab8421dcc1fe20
Giuseppe Barbieri <elect86@gmail.com>
2021-03-05 22:07:45 +0100

    build logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import kx.LwjglModules.jemalloc
 import kx.lwjglImplementation
 
 plugins {
-    val build = "0.4.1"
+    val build = "0.4.2"
     id("kx.kotlin.11") version build
     id("kx.lwjgl") version build
     id("kx.dokka") version build
