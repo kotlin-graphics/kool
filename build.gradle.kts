96027ba2ae6c26545a4448d8a5618e85bd1dc54d
Giuseppe Barbieri <elect86@gmail.com>
2021-03-20 08:43:17 +0100

    v0.9.0+17

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,15 @@ import kx.LwjglModules.jemalloc
 import kx.lwjglImplementation
 
 plugins {
-    val build = "0.6.4"
+    val build = "0.7.0+40"
     id("kx.kotlin.11") version build
     id("kx.lwjgl") version build
     id("kx.dokka") version build
-    id("kx.jitpack") version build
+    id("kx.publish") version build
     java
 }
 
-version = "0.9.0"
+version = "0.9.0+17"
 
 dependencies {
     lwjglImplementation(jemalloc)
