7ec3f837395a459c8c576201a4d082917afa2ef9
Giuseppe Barbieri <elect86@gmail.com>
2021-11-19 13:16:15 +0100

    0.9.3
    lwjgl 3.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,9 @@ plugins {
     kx("base" to "0.0.9",
        "publish" to "0.0.5",
        "utils" to "0.0.5")
-    id("org.lwjgl.plugin") version "0.0.18"
+    id("org.lwjgl.plugin") version "0.0.20"
 }
 
 dependencies {
-    Lwjgl {
-        release.`3_2_3`
-        snapshot
-        implementation(jemalloc) }
+    Lwjgl { implementation(jemalloc) }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 
 gradle.rootProject {
     group = "kotlin.graphics"
-    version = "0.9.2"
+    version = "0.9.3"
 }
 
 //includeBuild("../build-logic")
